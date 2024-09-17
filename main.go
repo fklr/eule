ecd9fd0e9b219bcff8b60eeaf0250f686cba296f
nova <146671001+ovnanova@users.noreply.github.com>
2024-09-16 23:14:31 -0700

    main function logic, set up some status handlers

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"github.com/bwmarrin/discordgo"
 )
-
-// "flag"
-// "fmt"
-// "os"
-// "os/signal"
-// "syscall"
-// "time"
-
-// "github.com/bwmarrin/discordgo"
 
 var purgeTasks = make(map[string]*PurgeTask)
 
@@ -20,6 +18,50 @@ type PurgeTask struct {
 	NextPurge time.Time
 }
 
-func main() {
+var startTime time.Time
 
+func main() {
+	var Token string
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+
+	startTime = time.Now()
+
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(ready)
+	dg.AddHandler(applicationCommandHandler)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+	s.UpdateGameStatus(0, "You look kind of familiar... have we met before?")
+}
+
+func applicationCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.Type != discordgo.InteractionApplicationCommand {
+		return
+	}
+
+	switch i.ApplicationCommandData().Name {
+	case "set_purge_interval":
+		//purge handler
+	case "status":
+		//status handler
+	}
 }
